ef8bce70f09cb5adc210c4c8c068c0576d6d2d0a
Fernando Espino <fernandozachary6857@iessineu.net>
2026-06-05 16:58:22 +0200

    Ex3 guardar datos en almacenamiento local Acabado

--- AWP5_EX3/script.js
+++ AWP5_EX3/script.js
@@ -6,8 +6,19 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -17,6 +28,7 @@ function updateSelectedCount() {
 
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
